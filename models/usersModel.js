b6ef8a63a7f56de39d1dab4afef3571abbe9cb97
baidu <baidu@baidudeMacBook-Pro.local>
2015-10-03 09:53:14 +0800

    add database

--- models/usersModel.js
+++ models/usersModel.js
@@ -4,7 +4,7 @@
  */
 var mysql = require('mysql');
 var conn = mysql.createConnection({
-    host: 'schoeu.com',
+    host: 'localhost',
     user: 'root',
     password: '1234',
     database: 'vs'
